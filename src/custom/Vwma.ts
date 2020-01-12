f0425ee42c0885601ecad5b844ed54636f3eb21b
Seamus Boyle <seamus@beantrader.com.au>
2020-01-13 08:53:05 +1100

    Port Vwma.js -> Vwma.ts

--- src/custom/Vwma.ts
+++ src/custom/Vwma.ts
@@ -1,57 +1,66 @@
-const obj = {
-  name: 'VWMA',
-  summary: 'Volume-Weighted Moving Average'
-};
 
-obj.info = function () {
-  obj.overlay = true;
+export default class Indicator implements Spock.Indicator {
+  name: string;
+  summary: string;
+  overlay: boolean;
+  curves: Spock.Curve[];
+  parameters: Spock.Parameter[];
+  requiredSource: Spock.RequiredSource;
 
-  obj._vwmaCurve = {
-    title: 'VWMA'
-  };
+  private maPeriods: Spock.IntParameter;
+  private cv: number[];
+  private v: number[];
 
-  obj.curves = [obj._vwmaCurve];
-
-  obj._maPeriods = {
-    name: 'VWMA periods',
-    defaultValue: 20
-  };
-
-  obj.parameters = [obj._maPeriods];
-  obj.requiredSource = Spock.RequiredSource.Bar;
-};
-
-obj.exec = function (period) {
-  const n = obj._maPeriods.value;
-
-  if (Plot.barUpdateMode === Spock.BarUpdateMode.AllBars) {
-    obj._cv = new Array(n);
-    obj._v = new Array(n);
-    // sums, rolling
-    /*
-    obj._cvS = new Array(n)
-    obj._vS = new Array(n)
-    */
+  constructor() {
+    this.name = 'VWMA2';
+    this.summary = 'Volume-Weighted Moving Average';
   }
 
-  const c0 = Plot.getTick(period);
-  const v0 = Plot.volume(period);
-  const cv0 = c0 * v0;
-  obj._cv[period] = cv0;
-  obj._v[period] = v0;
+  info = (): void => {
+    this.overlay = true;
+    this.curves = [{ title: 'VWMA' }];
 
-  // TODO(seamus): Use rolling window
-  if (period + 1 >= n) {
-    const start = period + 1 - n;
-    const end = period + 1;
-    const cv = obj._cv.slice(start, end);
-    const v = obj._v.slice(start, end);
-    const cvS = Plot.sum(cv, cv.length);
-    const vS = Plot.sum(v, v.length);
-    /*
-    obj._cvS[period] = cvS
-    obj._vS[period] = vS
-    */
-    return [cvS / vS];
-  }
-};
+    this.maPeriods = {
+      name: 'VWMA periods',
+      defaultValue: 20
+    };
+
+    this.parameters = [this.maPeriods];
+    this.requiredSource = Spock.RequiredSource.Bar;
+  };
+
+  exec = (period: number): number[] => {
+    const n = this.maPeriods.value;
+
+    if (Plot.barUpdateMode === Spock.BarUpdateMode.AllBars) {
+      this.cv = new Array(n);
+      this.v = new Array(n);
+      // sums, rolling
+      /*
+      this.cvS = new Array(n)
+      this.vS = new Array(n)
+      */
+    }
+
+    const c0 = Plot.getTick(period);
+    const v0 = Plot.volume(period);
+    const cv0 = c0 * v0;
+    this.cv[period] = cv0;
+    this.v[period] = v0;
+
+    // TODO(seamus): Use rolling window
+    if (period + 1 >= n) {
+      const start = period + 1 - n;
+      const end = period + 1;
+      const cv = this.cv.slice(start, end);
+      const v = this.v.slice(start, end);
+      const cvS = Plot.sum(cv, cv.length);
+      const vS = Plot.sum(v, v.length);
+      /*
+      this.cvS[period] = cvS
+      this.vS[period] = vS
+      */
+      return [cvS / vS];
+    }
+  };
+}
